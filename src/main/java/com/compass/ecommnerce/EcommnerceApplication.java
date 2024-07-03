7a75aea94a0be06dc3285867b5c8b2d9aecdf32d
João <joaokavalciuki@hotmail.com>
2024-07-03 13:22:58 -0300

    Projeto iniciado

---
+++ src/main/java/com/compass/ecommnerce/EcommnerceApplication.java
@@ -0,0 +1,13 @@
+package com.compass.ecommnerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommnerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommnerceApplication.class, args);
+	}
+
+}
